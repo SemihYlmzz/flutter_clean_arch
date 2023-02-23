f716c116a8150c981c9b7918230c4e530c570f68
SemihYlmzz <ekkotaymmm@gmail.com>
2023-02-23 11:45:46 +0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:water_application/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:water_application/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
 void main() {
   GetConcreteNumberTrivia(NumberTriviaRepository as NumberTriviaRepository)
--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_clean_arch/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_clean_arch/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:water_application/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:water_application/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:water_application/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
