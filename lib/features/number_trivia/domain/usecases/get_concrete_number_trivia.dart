233f7babf0ef0bbbec6652c082ed3e400cb6d3ea
SemihYlmzz <ekkotaymmm@gmail.com>
2023-02-23 12:38:36 +0300

    + Core/Usecases #  execute -> call

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,14 +1,16 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_arch/core/usecases/usecases.dart';
 import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
 import '../../../../core/error/failures.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, int> {
   final NumberTriviaRepository repository;
-  GetConcreteNumberTrivia(this.repository) : super();
+  GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>?> execute({required int number}) async {
+  @override
+  Future<Either<Failure, NumberTrivia>> call(int number) async {
     return await repository.getConcreteNumberTrivia(number);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
-void main() {
-  GetConcreteNumberTrivia(NumberTriviaRepository as NumberTriviaRepository)
-      .execute(number: 1);
-}
+void main() {}
